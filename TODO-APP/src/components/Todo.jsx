fa24589f75fff79b195f391d12fc546896737bd3
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-10-03 10:38:57 +0500

    added some functionality

--- TODO-APP/src/components/Todo.jsx
+++ TODO-APP/src/components/Todo.jsx
@@ -1,44 +1,72 @@
-import React, { useRef } from "react";
-import todo_icon from '../assets/todo_icon.png'
+import React, { useRef, useState } from "react";
+import todo_icon from "../assets/todo_icon.png";
 import TodoItems from "./TodoItems";
 
 const Todo = () => {
+  const [TodoList, setTodoList] = useState([]);
 
-  const inputRef = useRef()
+  const inputRef = useRef();
 
-  const add = ()=>{
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+
+  const deleteTodo = (id)=> {
+    setTodoList((prvTodos)=> {
+     return prvTodos.filter((todo) => todo.id !== id)
+
+    })
 
   }
 
-
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
+      {/*------title------ */}
 
+      <div className="flex items-center mt-7 gap-2">
+        <img className="w-8" src={todo_icon} alt="" />
+        <h1 className="text-3xl font-semibold">To-Do List</h1>
+      </div>
 
-{/*------title------ */}
+      {/*-----input Box ------ */}
 
-    <div className="flex items-center mt-7 gap-2">
-      <img className="w-8" src={todo_icon} alt="" />
-      <h1 className="text-3xl font-semibold">To-Do List</h1>
-    </div>
+      <div className="flex items-center my-7 bg-gray-200 rounded-full">
+        <input
+          ref={inputRef}
+          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          type="text"
+          placeholder="Add your task"
+        />
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
+          Add +
+        </button>
+      </div>
 
-    {/*-----input Box ------ */}
+      {/*-----Todo list------ */}
 
-    <div className="flex items-center my-7 bg-gray-200 rounded-full">
-      <input ref={inputRef} className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600" type="text" placeholder="Add your task" />
-      <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">Add +</button>
-    </div>
+      <div>
 
-    
-    {/*-----Todo list------ */}
-
-    <div>
-      <TodoItems text="Learn Coding"/>
-      <TodoItems text = "Learn Coding from YouTube"/>
-     
-    </div>
-    
-
+        {TodoList.map((item,index)=> {
+          return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete}deleteTodo={deleteTodo}/>
+        })}
+       
+      </div>
     </div>
   );
 };
